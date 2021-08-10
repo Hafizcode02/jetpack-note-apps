6efdcd524d5fbf2169320222dc6790f16f1b0c0e
HafiCode02 <hafizcode02@gmail.com>
2021-08-10 13:34:26 +0700

    add NoteRepository.kt

---
+++ app/src/main/java/com/hafizcode/noteappdicoding/repository/NoteRepository.kt
@@ -0,0 +1,33 @@
+package com.hafizcode.noteappdicoding.repository
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import com.hafizcode.noteappdicoding.database.Note
+import com.hafizcode.noteappdicoding.database.NoteDao
+import com.hafizcode.noteappdicoding.database.NoteRoomDatabase
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class NoteRepository(application: Application) {
+    private val mNotesDao: NoteDao
+    private val executorService: ExecutorService = Executors.newSingleThreadExecutor()
+
+    init {
+        val db = NoteRoomDatabase.getDatabase(application)
+        mNotesDao = db.noteDao()
+    }
+
+    fun getAllNotes(): LiveData<List<Note>> = mNotesDao.getAllNotes()
+
+    fun insert(note: Note) {
+        executorService.execute { mNotesDao.insert(note) }
+    }
+
+    fun update(note: Note) {
+        executorService.execute { mNotesDao.update(note) }
+    }
+
+    fun delete(note: Note) {
+        executorService.execute { mNotesDao.delete(note) }
+    }
+}
