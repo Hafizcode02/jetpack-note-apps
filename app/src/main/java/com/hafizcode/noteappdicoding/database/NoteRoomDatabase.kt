bd7f8839e4ffb8d1bf32d3af985201149031eed2
HafiCode02 <hafizcode02@gmail.com>
2021-08-13 13:55:34 +0700

    implement paging

--- app/src/main/java/com/hafizcode/noteappdicoding/database/NoteRoomDatabase.kt
+++ app/src/main/java/com/hafizcode/noteappdicoding/database/NoteRoomDatabase.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import java.util.concurrent.Executors
 
 @Database(entities = [Note::class], version = 1)
 abstract class NoteRoomDatabase : RoomDatabase() {
@@ -21,10 +23,29 @@ abstract class NoteRoomDatabase : RoomDatabase() {
                         context.applicationContext,
                         NoteRoomDatabase::class.java,
                         "note_database"
-                    ).build()
+                    ).addCallback(object : Callback() {
+                        override fun onCreate(db: SupportSQLiteDatabase) {
+                            addDummy()
+                        }
+                    })
+                        .build()
                 }
             }
             return INSTANCE as NoteRoomDatabase
+        }
+
+        private fun addDummy() {
+            Executors.newSingleThreadExecutor().execute {
+                val list: MutableList<Note> = ArrayList()
+                for (i in 0..9) {
+                    val dummyNote = Note()
+                    dummyNote.title = "Tugas $i"
+                    dummyNote.description = "Belajar Modul $i"
+                    dummyNote.date = "2021/01/01 09:09:09:0$i"
+                    list.add(dummyNote)
+                }
+                INSTANCE?.noteDao()?.insertAll(list)
+            }
         }
     }
 }
--- app/src/main/java/com/hafizcode/noteappdicoding/repository/NoteRepository.kt
+++ app/src/main/java/com/hafizcode/noteappdicoding/repository/NoteRepository.kt
@@ -2,6 +2,7 @@ package com.hafizcode.noteappdicoding.repository
 
 import android.app.Application
 import androidx.lifecycle.LiveData
+import androidx.paging.DataSource
 import com.hafizcode.noteappdicoding.database.Note
 import com.hafizcode.noteappdicoding.database.NoteDao
 import com.hafizcode.noteappdicoding.database.NoteRoomDatabase
@@ -17,7 +18,7 @@ class NoteRepository(application: Application) {
         mNotesDao = db.noteDao()
     }
 
-    fun getAllNotes(): LiveData<List<Note>> = mNotesDao.getAllNotes()
+    fun getAllNotes(): DataSource.Factory<Int, Note> = mNotesDao.getAllNotes()
 
     fun insert(note: Note) {
         executorService.execute { mNotesDao.insert(note) }
