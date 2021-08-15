4ca978f5192f5156713819e7c04edaf5b2184016
HafiCode02 <hafizcode02@gmail.com>
2021-08-15 23:09:58 +0700

    add menu to select sorted way

--- app/src/main/java/com/hafizcode/noteappdicoding/ui/main/MainActivity.kt
+++ app/src/main/java/com/hafizcode/noteappdicoding/ui/main/MainActivity.kt
@@ -3,6 +3,8 @@ package com.hafizcode.noteappdicoding.ui.main
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -22,6 +24,7 @@ class MainActivity : AppCompatActivity() {
     private val binding get() = _activityMainBinding
 
     private lateinit var adapter: NotePagedListAdapter
+    private lateinit var mainViewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,7 +32,7 @@ class MainActivity : AppCompatActivity() {
         _activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
-        val mainViewModel = obtainViewModel(this@MainActivity)
+        mainViewModel = obtainViewModel(this@MainActivity)
         mainViewModel.getAllNotes(SortUtils.NEWEST).observe(this, noteObserver)
 
         adapter = NotePagedListAdapter(this@MainActivity)
@@ -61,6 +64,23 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        var sort = ""
+        when (item.itemId) {
+            R.id.action_newest -> sort = SortUtils.NEWEST
+            R.id.action_oldest -> sort = SortUtils.OLDEST
+            R.id.action_random -> sort = SortUtils.RANDOM
+        }
+        mainViewModel.getAllNotes(sort).observe(this, noteObserver)
+        item.isChecked = true
+        return super.onOptionsItemSelected(item)
     }
 
     private fun showSnackbarMessage(message: String) {
